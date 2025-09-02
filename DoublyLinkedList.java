0e0d6555fd709c51a5a7e3d6641a9a1e0b81885b
pooja yadav <252poojayadav@gmail.com>
2025-09-02 22:03:08 +0530

    adding insertion and deletio in DLL

--- DoublyLinkedList.java
+++ DoublyLinkedList.java
@@ -11,6 +11,64 @@ class Node{
 
 public class DoublyLinkedList {
 
+    //deleting a node
+    private static Node delPos(Node head , int x){
+        //here x is position
+        if(head == null) return null;
+
+        Node curr = head;
+        int count =1;
+        while(curr != null && count < x){
+            curr = curr.next;
+            count++;
+        }
+
+        if(curr == null) return head;
+
+        if(curr == head){
+            head = head.next;
+            if(head != null) head.prev = null;
+            return head;
+        }
+
+        if(curr.prev != null){
+            curr.prev.next = curr.next;
+        }
+
+        if(curr.next != null){
+            curr.next.prev = curr.prev;
+        }
+        return head;
+    }
+
+    //insertion at DLL
+    private static Node insertAtPos(Node head, int x, int p){
+        //x - value p- position of element
+        Node newNode = new Node(x);
+
+        //check for empty -
+        if(head == null) return newNode;
+
+        Node curr = head;
+        int count =0;
+        
+        while(curr != null &&count < p){
+            curr = curr.next;
+            count++;
+        }
+
+        if(curr == null) return head;
+
+        newNode.next = curr.next;
+        newNode.prev = curr;
+
+        if(curr.next != null) curr.next.prev = newNode;
+
+        curr.next = newNode;
+
+        return head;
+
+    }
     //printing the double linked list
     private static  void printDLL(Node head){
         if(head == null) System.out.println("DLL is empty");;
@@ -47,10 +105,19 @@ public class DoublyLinkedList {
         int arr[] = {1,2,3,4,5};
 
         Node head = constructDLL(arr);
+        int x=6, p =3;
 
 
         System.out.println("Doubly linked list : " );
         printDLL(head);
+
+        Node newNode = insertAtPos(head, x, p);
+        System.out.println("After insertion of node at given pos : " );
+        printDLL(newNode);
+        System.out.println("After deleting the node : ");
+        int y = 3;
+        Node delNode = delPos(head, y);
+        printDLL(delNode);
 
     }
 }
